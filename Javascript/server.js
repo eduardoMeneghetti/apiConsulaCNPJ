1ac68fc5133b7b73c40f80547c7afdbe4f863f74
eduardoMeneghetti <132016082+eduardoMeneghetti@users.noreply.github.com>
2023-05-30 17:44:22 -0300

    versão 1.2
    Implementação de campo de busca, além de uma função de retorno dos dados no HTML.

--- Javascript/server.js
+++ Javascript/server.js
@@ -1,11 +1,23 @@
-fetch('https://brasilapi.com.br/api/cnpj/v1/{89131189000165}?',{
-    method: 'GET'
-})
-.then(response => response.json())
-.then(data => {
-    data = window.document.getElementById('res')
-    console.log(window.location.href)
-})
-.catch(error => {
-    console.error(error);
-});
+function buscaCNPJ() {
+    var cnpj = document.getElementById('cnpj').value;
+    var res = document.getElementById('res');
+    fetch('https://brasilapi.com.br/api/cnpj/v1/'+cnpj+'?', {
+        method: 'GET'
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Criar uma representação em formato de string dos dados
+        var dataString = '';
+        for (var key in data) {
+            if (data.hasOwnProperty(key)) {
+                dataString += key + ': ' + JSON.stringify(data[key]) + '<br>';
+            }
+        }
+        // Atribuir a string ao elemento HTML
+        res.innerHTML = dataString;
+    })
+    .catch(error => {
+        console.error(error);
+    });
+}
+
